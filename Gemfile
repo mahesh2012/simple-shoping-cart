b0fb269ef0aeaeeb74e56bd69a281b27bd0e91a1
Mahesh <mahesh@elumitas.com>
2021-11-13 16:47:21 +0530

    Add seeded Data

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'simple_command'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 gem 'dotenv-rails'
+gem 'seed_dump'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {name: "Mahesh", email: "jmaheshkumar2@gmail.com", phone: nil, address: nil, is_admin: true, password: "Mahesh@123", password_confirmation: "Mahesh@123"}
+])
+ProductImage.create!([
+  {image_url: "http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/mens-better-than-naked-jacket-AVMH_LC9_hero.png", product_id: 1},
+  {image_url: "http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/womens-better-than-naked-jacket-AVKL_NN4_hero.png", product_id: 2},
+  {image_url: "http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/womens-single-track-shoe-ALQF_JM3_hero.png", product_id: 3},
+  {image_url: "http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/enduro-boa-hydration-pack-AJQZ_JK3_hero.png", product_id: 4},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567048-13938600-aa33-11e9-9cfd-712191013192.jpeg", product_id: 5},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567049-13938600-aa33-11e9-9c69-a4184bf8e524.jpeg", product_id: 6},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567053-13938600-aa33-11e9-9780-104fe4019659.png", product_id: 7},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567051-13938600-aa33-11e9-8ae7-0b5c19aafab4.jpeg", product_id: 8},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567054-13938600-aa33-11e9-9163-eec98e239b7a.png", product_id: 9},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567055-142c1c80-aa33-11e9-96ff-9fbac6413625.png", product_id: 10},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567056-142c1c80-aa33-11e9-8682-10065d338145.png", product_id: 11},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567052-13938600-aa33-11e9-9a88-cd842073ba44.jpg", product_id: 12},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567060-142c1c80-aa33-11e9-8188-5a4803844a9e.png", product_id: 13},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567059-142c1c80-aa33-11e9-939b-2ecf4492786d.png", product_id: 14},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567061-14c4b300-aa33-11e9-9fee-63ff2c0c9823.png", product_id: 15},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567062-14c4b300-aa33-11e9-9dcd-8bfed4ece810.png", product_id: 16},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567063-14c4b300-aa33-11e9-8515-bcb866da9ea3.png", product_id: 17},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567057-142c1c80-aa33-11e9-9781-9e442418eaab.png", product_id: 18},
+  {image_url: "https://user-images.githubusercontent.com/41929050/61567058-142c1c80-aa33-11e9-89fb-b4f30d84d69d.png", product_id: 19}
+])
+Product.create!([
+  {name: "MEN'S BETTER THAN NAKED & trade;JACKET ", price: "198.0"},
+  {name: "WOMEN'S BETTER THAN NAKED&trade; JACKET", price: "667.0"},
+  {name: "WOMEN'S SINGLE-TRACK SHOE", price: "801.0"},
+  {name: "Enduro Boa&reg; Hydration Pack", price: "786.0"},
+  {name: "Jenlooper-cactus", price: "201.0"},
+  {name: "Jenlooper-light", price: "596.0"},
+  {name: "Jenlooper-lightshow", price: "880.0"},
+  {name: "Jenlooper-survival", price: "889.0"},
+  {name: "Sailorhg-bubblesortpic", price: "197.0"},
+  {name: "Sailorhg-corsage", price: "531.0"},
+  {name: "Sailorhg-kit", price: "601.0"},
+  {name: "Sailorhg-led", price: "211.0"},
+  {name: "Selinazawacki-soi-shirt", price: "191.0"},
+  {name: "Selinazawacki-soi-pins", price: "937.0"},
+  {name: "Vogueandcode-hipster-dev-bro", price: "118.0"},
+  {name: "Vogueandcode-pretty-girls-code-tee", price: "295.0"},
+  {name: "Vogueandcode-ruby-sis-2", price: "615.0"},
+  {name: "Selinazawacki-moon", price: "417.0"},
+  {name: "Selinazawacki-shirt", price: "849.0"}
+])
